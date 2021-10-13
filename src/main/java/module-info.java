eb570dca4e81120cc42f818c9518a6a321864107
ardhan <ardhan.s.pratama@gmail.com>
2021-10-13 09:43:06 +0800

    Added LoginUI.fxml and LoginController.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,6 @@
-module com.mindblankprescriptionjavafx {
+module com.mindblank {
     requires javafx.controls;
     requires javafx.fxml;
-
 
     opens com.mindblank to javafx.fxml;
     exports com.mindblank;
