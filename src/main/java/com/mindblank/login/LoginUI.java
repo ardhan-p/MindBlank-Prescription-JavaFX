67c887a958c454c928d399d54ad24cec864d4b94
ardhan <ardhan.s.pratama@gmail.com>
2021-10-14 17:54:16 +0800

    Added all JUnit tests for user login

--- src/main/java/com/mindblank/login/LoginUI.java
+++ src/main/java/com/mindblank/login/LoginUI.java
@@ -16,16 +16,25 @@ public class LoginUI {
     @FXML
     private Label loginStatus;
 
+    public TextField getUsernameInput() {
+        return usernameInput;
+    }
+
     public void clear(ActionEvent event) {
         usernameInput.setText("");
         passwordInput.setText("");
         loginStatus.setText("Username and password cleared!");
     }
 
+
     public void onSubmit(ActionEvent event) {
         if (LoginController.validateUser(usernameInput.getText(), passwordInput.getText())) {
             //TODO: add code to allow users to access main menu
             loginStatus.setText("Login details are correct!");
+        } else if (usernameInput.getText().trim().isEmpty()) {
+            loginStatus.setText("Username is required");
+        } else if (passwordInput.getText().trim().isEmpty()) {
+            loginStatus.setText("Password is required");
         } else {
             loginStatus.setText("Login details are incorrect! Try again.");
         }
--- src/test/java/com/mindblank/test/LoginTest.java
+++ src/test/java/com/mindblank/test/LoginTest.java
@@ -9,8 +9,36 @@ import static org.junit.jupiter.api.Assertions.*;
 class LoginTest {
 
     @Test
-    @DisplayName("Login user YoMama")
-    void login() {
+    @DisplayName("TC-1-2")
+    void loginEmptyUsername() {
+        User testUser = new User("", "sofat123");
+        assertEquals(false, testUser.login(testUser.getuName(), testUser.getuPass()));
+    }
+
+    @Test
+    @DisplayName("TC-1-3")
+    void loginEmptyPassword() {
+        User testUser = new User("YoMama", "");
+        assertEquals(false, testUser.login(testUser.getuName(), testUser.getuPass()));
+    }
+
+    @Test
+    @DisplayName("TC-1-4")
+    void loginIncorrectPassword() {
+        User testUser = new User("YoMama", "sofat234");
+        assertEquals(false, testUser.login(testUser.getuName(), testUser.getuPass()));
+    }
+
+    @Test
+    @DisplayName("TC-1-5")
+    void loginIncorrectUsername() {
+        User testUser = new User("yomama", "sofat123");
+        assertEquals(false, testUser.login(testUser.getuName(), testUser.getuPass()));
+    }
+
+    @Test
+    @DisplayName("TC-1-6")
+    void loginCorrectUser() {
         User testUser = new User("YoMama", "sofat123");
         assertEquals(true, testUser.login(testUser.getuName(), testUser.getuPass()));
     }
