e927b65ae8fa369a686aa61a028c03833d3374fe
ardhan <ardhan.s.pratama@gmail.com>
2021-11-09 01:53:22 +0800

    Changed SQL statements in yml

--- src/main/java/com/mindblank/DatabaseConnection.java
+++ src/main/java/com/mindblank/DatabaseConnection.java
@@ -9,7 +9,7 @@ public class DatabaseConnection {
     public Connection getConnection() {
         String databaseName = "mindblank_db";
         String databaseUser = "root";
-        String databasePassword = "password123";
+        String databasePassword = "root";
         String url = "jdbc:mysql://localhost/" + databaseName;
 
         try {
