e61f9d3c02b262a6fea28ecb6fb141735ed81877
ardhan <ardhan.s.pratama@gmail.com>
2021-11-11 20:33:16 +0800

    Changed code format for all users' logout

---
+++ src/main/java/com/mindblank/admin/controllers/AdminController.java
@@ -0,0 +1,11 @@
+package com.mindblank.admin.controllers;
+
+import com.mindblank.entities.Admin;
+
+public class AdminController {
+    private Admin admin;
+
+    public AdminController(Admin admin) {
+        this.admin = admin;
+    }
+}
